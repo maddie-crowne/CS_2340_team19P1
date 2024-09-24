52b73de520d84e3ee19eac8d6ef19e4feba81cc6
Katherine Sanders <68821698+katherinesanders@users.noreply.github.com>
2024-09-24 12:04:39 -0600

    Update views.py
    add in all functions for registering, logging in, and saving account info along with necessary imports

--- googleMaps/views.py
+++ googleMaps/views.py
@@ -1,7 +1,57 @@
-from django.shortcuts import render
 from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth import logout
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from .models import Favorite
 import requests
+from django.shortcuts import redirect
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username is already taken')
+            else:
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+                login(request, user)
+                return redirect('home')
+        else:
+            messages.error(request, 'Passwords do not match')
+
+    return render(request, 'registration/register.html')
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+
+    return render(request, 'registration/login.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+def account_info(request):
+    favorites = Favorite.objects.filter(user=request.user)
+    return render(request, 'account_info.html', {'favorites': favorites})
+
 
 # Create your views here.
 def googleMaps(request):
