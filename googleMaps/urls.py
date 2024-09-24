6d323d3758bcf6d2aff3b495cc1105a3d6d260aa
Katherine Sanders <68821698+katherinesanders@users.noreply.github.com>
2024-09-24 12:02:29 -0600

    Update urls.py
    add in path to account info

--- googleMaps/urls.py
+++ googleMaps/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'googleMaps'
 urlpatterns = [
     path('', views.googleMaps, name='googleMaps'),
+    path('account/', views.account_info, name='account_info'),  # User's account info page
 ]
