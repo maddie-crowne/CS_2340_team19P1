a61ea27e643dd457c942af8594e3d9f9d8cd018d
Katherine Sanders <68821698+katherinesanders@users.noreply.github.com>
2024-09-24 18:33:24 -0600

    Update urls.py

--- googleMaps/urls.py
+++ googleMaps/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'googleMaps'
+app_name = 'googleMaps'  # Namespacing the app for better URL organization
 urlpatterns = [
     path('', views.googleMaps, name='googleMaps'),
+    path('register/', views.register, name='register'),
     path('account/', views.account_info, name='account_info'),  # User's account info page
 ]
