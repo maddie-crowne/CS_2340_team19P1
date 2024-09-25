7fe660da52bbdb6191ad7bd2a3e720559265a117
Katherine Sanders <68821698+katherinesanders@users.noreply.github.com>
2024-09-24 18:32:10 -0600

    Create forms.py

---
+++ auth/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'password1', 'password2')
