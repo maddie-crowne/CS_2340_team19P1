cd63a72ce2e0cef670da4bce877732337d819004
Samrita Shetty <SamritaSShetty@gmail.com>
2024-10-02 04:40:46 -0400

    Final

--- AtlantaFoodFinder/urls.py
+++ AtlantaFoodFinder/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('', lambda request: redirect('googleMaps/')),  # Redirect root to googleMaps
     path('googleMaps/', include('googleMaps.urls')),
     path('auth/', include('auth.urls')),
     path('admin/', admin.site.urls),
